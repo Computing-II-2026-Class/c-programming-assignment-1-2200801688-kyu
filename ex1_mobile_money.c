61537b63aabca349af9e22c7fa014bcd72e9b4f2
2200801688-kyu <2200801688@std.kyu.ac.ug>
2026-03-25 01:30:27 +0300

    Update ex1_mobile_money.c

--- ex1_mobile_money.c
+++ ex1_mobile_money.c
@@ -1,11 +1,21 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: MUKINE JOEL */
+/* Student Number: 22/U/BIO/1688/PD */
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+    float amount,fee_percebt,fee_amount,total_deducted;
+    printf("Enter amount to send:");
+    scanf("%f",&amount);
+    printf(Enter transaction fee(%):");
+    scanf("%f",&fee_percent);
+    
+    fee_amount = amount*(fee_percent)/100;
+    total_deducted = amount+fee_amount;
+    printf("Transaction fee:%.2f UGX\n",fee_amount);
+    printf("Total_deducted:%.2f UGX\n",total_deducted(;
+
 
     return 0;
 }
