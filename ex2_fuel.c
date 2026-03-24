7c0228c2376e8f416d3cc575b532dfd7d571434e
2200801688-kyu <2200801688@std.kyu.ac.ug>
2026-03-25 01:43:00 +0300

    Update ex2_fuel.c

--- ex2_fuel.c
+++ ex2_fuel.c
@@ -1,11 +1,19 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: MUKINE JOEL*/
+/* Student Number: 22/U/BIO/1688/PD */
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+float distance,fuel,fuel_efficiency;
+    printf("Enter distance in (km):");
+    scanf("%f",& distance);
+    printf("Enter fuel in (litres):");
+    scanf("%f",&fuel);
+
+    fuel_efficirncy = distance/fuel;
+    prinf("fuel_efficiency:%.2f(km/l)\n",fuel_efficiency);
+    
 
     return 0;
 }
