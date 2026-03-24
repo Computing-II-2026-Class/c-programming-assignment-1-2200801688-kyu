bb5eca9b38938d3622d1adad6c0d347d118f3fdd
2200801688-kyu <2200801688@std.kyu.ac.ug>
2026-03-25 01:53:39 +0300

    Update ex3_electricity.c

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -1,11 +1,18 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: MUKINE JOEL*/
+/* Student Number: 22/U/BIO/1688/PD */
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+    float units,cost_per_unit,total_electricity_bill;
+    prinf("Enter units:");
+    scanf("%f",&units);
+    prinf("Enter cost_per_unit:");
+    scanf("%f",&cost_per_unit:);
+    
+    total_electricity_bill = units * cost_per_unit;
+    printf("total_electricity_bill:%.2f UGX\n",total_electricity_bill);
 
     return 0;
 }
